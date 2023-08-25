fa49d58cb884fe1ecb52885f5a13ec02e058a412
Simon Vargas <sjvargas01@gmail.com>
2023-08-25 00:13:35 -0400

    first commit

---
+++ src/main/java/com/example/headers/HeadersApplication.java
@@ -0,0 +1,13 @@
+package com.example.headers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadersApplication.class, args);
+	}
+
+}
